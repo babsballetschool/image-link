120b7f7d86c249bf292595a9dc9fbd90d13c5580
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-08-15 11:10:26 +0200

    Show a markdown link of image

--- src/ImageLink.elm
+++ src/ImageLink.elm
@@ -4,11 +4,24 @@ import Html
 
 
 main =
-    Html.text "Hello, World!"
+    let
+        image =
+            createImage "resources/image/algemeen/spitzen.jpg"
+                |> withDescription "Spitzen aan de bar"
+
+        model =
+            { image = Just image }
+    in
+        view model
 
 
 
 -- Model
+
+
+type alias Model =
+    { image : Maybe Image
+    }
 
 
 type Image
@@ -17,10 +30,34 @@ type Image
         , description : Maybe String
         }
 
+
 createImage : String -> Image
 createImage source =
     Image { source = source, description = Nothing }
 
+
 withDescription : String -> Image -> Image
 withDescription description (Image image) =
     Image { image | description = Just description }
+
+
+
+-- View
+
+
+view : Model -> Html.Html msg
+view model =
+    case model.image of
+        Just image ->
+           link image
+
+        Nothing ->
+            Html.text "select an image"
+
+
+link : Image -> Html.Html msg
+link (Image { source, description }) =
+    let
+        text = "![](" ++ source ++ ")"
+    in
+        Html.pre [] [Html.text text]

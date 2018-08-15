c4fae8c85fb13ce79c539d69ddf0792a4ad9616b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-08-15 10:35:28 +0200

    Create a Image data type

--- src/ImageLink.elm
+++ src/ImageLink.elm
@@ -2,4 +2,16 @@ module ImageLink exposing (..)
 
 import Html
 
-main = Html.text "Hello, World!"
+
+main =
+    Html.text "Hello, World!"
+
+
+
+-- Model
+
+
+type alias Image =
+    { source : String
+    , description : Maybe String
+    }
